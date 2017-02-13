e41c461c1c1e9e224d978370816166aa92b33cf3
James Dunham <james@jdunham.io>
2017-02-13 23:12:55 +0000

    version 0.0.2

---
+++ R/targets.r
@@ -0,0 +1,13 @@
+#' \code{dgo} example data: U.S. population targets
+#'
+#' A table giving U.S. population proportions by year for combinations of
+#' demographic variables. For use with the \code{target_data} argument of
+#' function \code{shape} in package '\href{https://jdunham.io/dgo/}{dgo}'. Data
+#' are from the U.S. Census.  Proportions sum to to 1 within years.
+#'
+#' @docType data
+#' @name targets
+#' @usage targets
+#' @format A \code{data.frame} with ~803K observations of 10 variables.
+#' @seealso \url{http://www.census.gov/}
+NULL
